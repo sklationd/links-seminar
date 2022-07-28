17121ea5908c832dba894e9a2f820c6c772a447d
sklationd <sklationd@gmail.com>
2022-07-28 10:50:49 +0900

    feat: post image

--- frontend/src/handler.js
+++ frontend/src/handler.js
@@ -1,5 +1,9 @@
+import { ADDRESS } from "./address.js";
+import { ABI } from "./abi.js";
+
 export const registerHandler = function (provider, network, currentAccount) {
     registerConnectButtonHandler(provider);
+    registerSubmitButtonHandler(provider, network, currentAccount);
 };
 
 const registerConnectButtonHandler = function (provider) {
@@ -11,4 +15,34 @@ const registerConnectButtonHandler = function (provider) {
             console.error(e);
         }
     });
+};
+
+const registerSubmitButtonHandler = function (
+    provider,
+    network,
+    currentAccount
+) {
+    if (currentAccount && ADDRESS[network.name]) {
+        const abi = ABI;
+        const address = ADDRESS[network.name]; // TODO check chainId and network name
+        const contract = new ethers.Contract(
+            address,
+            abi,
+            provider.getSigner()
+        );
+
+        const form = document.getElementById("imageUrlForm");
+        const input = document.getElementById("imageUrlInput");
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            // TODO input check
+            if (input.value.length > 0) {
+                try {
+                    contract.addImage(input.value);
+                } catch (error) {
+                    console.log(error.message, `Failed to post images`);
+                }
+            }
+        });
+    }
 };
