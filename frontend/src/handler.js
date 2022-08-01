f231949d5c54b6ff02329833d6e0fcb348d1a315
sklationd <sklationd@gmail.com>
2022-08-01 17:56:41 +0900

    fix

--- frontend/src/handler.js
+++ frontend/src/handler.js
@@ -50,8 +50,10 @@ const registerSubmitButtonHandler = function (
 
 const registerImageAddedEventListener = function (provider, network) {
     const contract = new ethers.Contract(ADDRESS[network.name], ABI, provider);
-    contract.once("ImageAdded", (author, url, id) => {
-        console.log(`${author} posted ${id}th image ${url}`);
-        window.location.reload();
+    provider.once("block", () => {
+        contract.on("ImageAdded", (author, url, id) => {
+            console.log(`${author} posted ${id}th image ${url}`);
+            window.location.reload();
+        });
     });
 };
